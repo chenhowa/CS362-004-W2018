a8de1b72e255d5e81e4431834aecc67e764c6100
Howard Chen <H_Chen@alumni.baylor.edu>
2018-02-03 15:33:47 -0800

    Complete code coverage and measurement of CPU time

--- projects/chenhowa/random_quiz/testme.c
+++ projects/chenhowa/random_quiz/testme.c
@@ -1,18 +1,80 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 char inputChar()
 {
-    // TODO: rewrite this function
-    return ' ';
+    //To generate random input char, get an integer between 
+    int code;
+    char c;
+
+    code = rand() % 14;
+
+    switch(code) {
+        case 0:
+            c = '[';
+            break;
+        case 1:
+            c = '(';
+            break;
+        case 2:
+            c = '{';
+            break;
+        case 3:
+            c = ' ';
+            break;
+        case 4:
+            c = 'a';
+            break;
+        case 5:
+            c = 'x';
+            break;
+        case 6:
+            c = '}';
+            break;
+        case 7:
+            c = ')';
+            break;
+        case 8:
+            c = 'r';
+            break;
+        case 9:
+            c = 'e';
+            break;
+        case 10:
+            c = 's';
+            break;
+        case 11:
+            c = 't';
+            break;
+        case 12:
+            c = '\0';
+            break;
+        default: 
+            c = ']';
+            break;
+    }
+
+    return c;
 }
+
+char* string = NULL;
 
 char *inputString()
 {
-    // TODO: rewrite this function
-    return "";
+    int i;
+    if(string != NULL) {
+        free(string);    
+    }
+    string = malloc(7 * sizeof(char));
+
+    for(i = 0; i < 6; i++) {
+        string[i] = inputChar();
+    } 
+    string[6] = '\0'; //Make sure the string is null terminated
+
+    return string;
 }
 
 void testme()
@@ -21,6 +83,11 @@ void testme()
   char *s;
   char c;
   int state = 0;
+  clock_t begin;
+  clock_t end;
+  double time_spent;
+
+  begin = clock();
   while (1)
   {
     tcCount++;
@@ -42,7 +109,15 @@ void testme()
        && s[4] == 't' && s[5] == '\0'
        && state == 9)
     {
+      end = clock();
+      time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+      printf("\nTime spent until full coverage: %f\n", time_spent);
       printf("error ");
+
+      if(string != NULL) {
+        free(string); 
+      }
+
       exit(200);
     }
   }
@@ -53,5 +128,6 @@ int main(int argc, char *argv[])
 {
     srand(time(NULL));
     testme();
+
     return 0;
 }
