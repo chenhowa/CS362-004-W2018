e389d3d832dca33eced2b5296f3a7e0eef72602d
Howard Chen <H_Chen@alumni.baylor.edu>
2018-02-02 19:19:53 -0800

    Added unit testing for adventurerEffect function

--- projects/chenhowa/dominion/dominion.c
+++ projects/chenhowa/dominion/dominion.c
@@ -1235,9 +1235,10 @@ int updateCoins(int player, struct gameState *state, int bonus)
 
 int adventurerEffect (struct gameState *state, int currentPlayer) {
     int cardDrawn; //tracks the id of the drawn card
-    int drawntreasure = 1; //counts how much treasure the player has drawn
+    int drawntreasure = 0; //counts how much treasure the player has drawn
     int temphand[MAX_HAND];
     int z = 0; //This is a counter for the temp hand
+    int loopCount = 0;
     while(drawntreasure<2){
         if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
             shuffle(currentPlayer, state);
@@ -1251,6 +1252,12 @@ int adventurerEffect (struct gameState *state, int currentPlayer) {
             state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
             z++;
         }
+
+        if(loopCount > 1) {
+            return -1; 
+        }
+
+        loopCount++;
     }
     while(z-1>=0){
         state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
--- projects/chenhowa/dominion/unit_tests/unittest6.c
+++ projects/chenhowa/dominion/unit_tests/unittest6.c
@@ -8,16 +8,15 @@
  */
 
 
-#include "dominion.h"
-#include "dominion_helpers.h"
+#include "../dominion.h"
+#include "../dominion_helpers.h"
 #include <string.h>
 #include <stdio.h>
-#include "assertions.h"
-#include "rngs.h"
+#include "../assertions.h"
+#include "../rngs.h"
 #include <stdlib.h>
 
 
-//Contains all tests for numHandCards()
 int testAdventurer() {
     //Declaration of testing-specific variables
     char *description;
@@ -26,9 +25,6 @@ int testAdventurer() {
     int return_value;
     int player;
     int condition;
-    int handCount;
-    int card;
-    int difference;
     int i;
 
     //Declarations to initialize game
@@ -36,55 +32,159 @@ int testAdventurer() {
     int kingdomCards[10] = {adventurer, embargo, village, minion, mine, cutpurse,
 			sea_hag, tribute, smithy, council_room};
     int seed = 100;
-    struct gameState preState, postState;
+    struct gameState preState, postState, initialState;
 
     //First, initialize the game
-    initializeGame(numPlayers, kingdomCards, seed, postState);
+    initializeGame(numPlayers, kingdomCards, seed, &postState);
 
+    //Save the initial state of the game
+    memcpy(&initialState, &postState, sizeof(struct gameState));
+    player = postState.whoseTurn;
 
-    /* THIS TEST MIGHT FAIL BECAUSE NO TREASURE CARDS WERE YET BOUGHT */
-
-    /************** TEST 1: return value is correct *****************/
-    description = "Return value is correct";
-    player = postState->whoseTurn;
-    memcpy(&preState, &postState, sizeof(struct gameState));
-    return_value = adventurerEffect(postState, player)
-    didAllPass = didAllPass && assertTrue(return_value == 0, description); 
-
-    /************* TEST 2: handCount is greater by 2 *****************/
-    description = "hand has incremented by 2 cards";
-    didAllPass = didAllPass && assertTrue(
-                    preState.handCount[player] + 2 == postState.handCount[player],
-                        description);
-
-    /************ TEST 3: discardCount += (x - 2), deckCount -= (x) ***/
-    description = "discardCount += (x - 2), deckCount -= (x)";
-    condition = (preState.deckCount[player] - postState.deckCount[player])
-                == (postState.discardCount[player] - preState.discardCount[player] + 2);
-    didAllPass = didAllPass && assertTrue(condition, description);
-
-    /************ TEST 4: New cards in Player's hands are treasures ***/
-    description = "Player's two new cards are Treasure cards";
-    handCount = postState.handCount[player];
-    card = postState.hand[player][handCount - 1];
-    condition = card == copper || card == silver || card == gold;
-    card = postState.hand[player][handCount - 2];
-    condition = condition &&
-                (card == copper || card == silver || card == gold);
-    didAllPass = didAllPass && assertTrue(condition, description);
-
-    /*********** TEST 5: No other changes occurred *****/
-    description = "No other changes occurred";
-
-    /**** To do this, I need to copy things from the preState into the
-     * postState, from handcount, deckCount, and discardCount to the actual
-     * cards in preState */
-
-    /*** FINISH LATER ***/
-
-
-
+    /********** TEST 1: When top two cards in deck are Treasures copper and silver ********/
+    // Set up game
+    postState.deckCount[player] = 5; //To avoid tons of code, just use a deck of 5.
+    for(i = 0; i < postState.deckCount[player]; i++) {
+        postState.deck[player][i] = adventurer; 
+    } 
+    postState.deck[player][postState.deckCount[player] - 1] = copper;
+    postState.deck[player][postState.deckCount[player] - 2] = silver;
     
+    //Save this state for later comparison.
+    memcpy(&preState, &postState, sizeof(struct gameState));
+
+    description = "Test 1: return value is correct";
+    return_value = adventurerEffect(&postState, player);
+    didAllPass = assertTrue(return_value == 0, description) && didAllPass;
+
+    description = "Test 1: player's deck has two fewer cards";
+    didAllPass = assertTrue(postState.deckCount[player] == preState.deckCount[player] - 2,
+                    description) && didAllPass;
+
+    description = "Test 1: player's hand has two more cards";
+    didAllPass = assertTrue(postState.handCount[player] == preState.handCount[player] + 2, 
+                    description) && didAllPass;
+
+    description = "Test 1: Those two new cards are the copper and silver";
+    condition = postState.hand[player][postState.handCount[player] - 1] == silver &&
+                postState.hand[player][postState.handCount[player] - 2] == copper;
+    didAllPass = assertTrue(condition, description) && didAllPass;
+
+    description = "Test 1: No other changes were made";
+    preState.deckCount[player] -= 2;
+    preState.handCount[player] += 2;
+    preState.hand[player][preState.handCount[player] - 1] = silver;
+    preState.hand[player][preState.handCount[player] - 2] = copper;
+    mem_cmp_return = memcmp(&preState, &postState, sizeof(struct gameState));
+    didAllPass = assertTrue(mem_cmp_return == 0, description);
+
+    // Restore postState to initial condition 
+    memcpy(&postState, &initialState, sizeof(struct gameState));
+
+    /*************** TEST 2: Bottom two cards are treasures silver and gold,
+     *                  The top 3 are all adventurers ***/
+    postState.deckCount[player] = 5;
+    postState.deck[player][0] = silver;
+    postState.deck[player][1] = gold;
+    postState.deck[player][2] = adventurer;
+    postState.deck[player][3] = adventurer;
+    postState.deck[player][4] = adventurer;
+
+    //Save state for later comparison.
+    memcpy(&preState, &postState, sizeof(struct gameState));
+
+    description = "Test 2: Return value is correct";
+    return_value = adventurerEffect(&postState, player);
+    didAllPass = assertTrue(return_value == 0, description) && didAllPass;
+
+    description = "Test 2: player's hand has two more cards";
+    didAllPass = assertTrue(postState.handCount[player] == preState.handCount[player] + 2, 
+                    description) && didAllPass;
+
+    description = "Test 2: Those two new cards are the gold and silver";
+    condition = postState.hand[player][postState.handCount[player] - 1] == silver &&
+                postState.hand[player][postState.handCount[player] - 2] == gold;
+    didAllPass = assertTrue(condition, description) && didAllPass;
+
+    description = "Test 2: player's deck now has no cards at all";
+    didAllPass = assertTrue(postState.deckCount[player] == 0, description) && didAllPass;
+
+    description = "Test 2: discardCount has 3 more cards";
+    didAllPass = assertTrue(postState.discardCount[player] ==
+                preState.discardCount[player] + 3, description) && didAllPass;
+    
+    description = "Test 2: the three discarded cards are all adventurers";
+    condition = postState.discard[player][postState.discardCount[player] - 1] == adventurer &&
+        postState.discard[player][postState.discardCount[player] - 2] == adventurer &&
+        postState.discard[player][postState.discardCount[player] - 3] == adventurer;
+    didAllPass = assertTrue(condition, description) && didAllPass;
+
+    description = "Test 2: No other changes were made";
+    preState.deckCount[player] = 0;
+    preState.discardCount[player] += 3;
+    preState.discard[player][preState.discardCount[player] - 1] = adventurer;
+    preState.discard[player][preState.discardCount[player] - 2] = adventurer;
+    preState.discard[player][preState.discardCount[player] - 3] = adventurer;
+    mem_cmp_return = memcmp(&preState, &postState, sizeof(struct gameState));
+    didAllPass = assertTrue(mem_cmp_return == 0, description) && didAllPass;
+
+    //restore postState to initial state.
+    memcpy(&postState, &initialState, sizeof(struct gameState));
+
+    /***************** Test 3: 1 card (gold) in deck **********/
+    postState.deckCount[player] = 5;
+    for(i = 0; i < postState.deckCount[player]; i++) {
+        postState.deck[player][i] = adventurer; 
+    } 
+    postState.deck[player][postState.deckCount[player] - 1] = gold;
+    memcpy(&preState, &postState, sizeof(struct gameState));
+
+    description = "Test 3: Return value is correct, proving no infinite loop possible";
+    return_value = adventurerEffect(&postState, player);
+    didAllPass = assertTrue(return_value == 0, description) && didAllPass;
+
+    description = "Test 3: Player's handCount has increased by 1";
+    didAllPass = assertTrue(postState.handCount[player] == preState.handCount[player] + 1,
+                description) && didAllPass;
+
+    description = "Test 3: The new card is the gold";
+    didAllPass = assertTrue(postState.hand[player][postState.handCount[player] - 1] == gold,
+                description) && didAllPass;
+
+    //Can't test that no other changes were made or the state of the discard pile, as adventurerEffect contains shuffle,
+    // which randomizes the game state a whoooole bunch
+
+    // //restore postState to initial state.
+    memcpy(&postState, &initialState, sizeof(struct gameState));
+
+    /*************** Test 4: 1 card (copper) in deck, 1 card (gold) in discard pile **/
+    postState.deckCount[player] = 5;
+    for(i = 0; i < postState.deckCount[player]; i++) {
+        postState.deck[player][i] = adventurer; 
+    } 
+    postState.deck[player][postState.deckCount[player] - 1] = copper;
+    postState.discardCount[player] = 1;
+    postState.discard[player][0] = gold;
+    memcpy(&preState, &postState, sizeof(struct gameState));
+
+    description = "Test 4: Return value is correct, proving no infinite loop possible";
+    return_value = adventurerEffect(&postState, player);
+    didAllPass = assertTrue(return_value == 0, description) && didAllPass;
+
+    description = "Test 4: Player's handCount has increased by 2";
+    didAllPass = assertTrue(postState.handCount[player] == preState.handCount[player] + 2,
+                description) && didAllPass;
+
+    description = "Test 4: The new cards are the gold and copper";
+    condition = postState.hand[player][postState.handCount[player] - 1] == gold &&
+                postState.hand[player][postState.handCount[player] - 2] == copper;
+    didAllPass = assertTrue(condition, description) && didAllPass;
+
+    /* Can't test discard pile or that no other changes were made, as the shuffle function in adventurerEffect randomizes the game state a wwhoooooole bunch 
+     */
+
+    // //restore postState to initial state.
+    memcpy(&postState, &initialState, sizeof(struct gameState));
 
     return didAllPass;
 }
@@ -100,6 +200,7 @@ int main(int argc, char *argv[])
     } else {
         printf("adventurerEffect(): WARNING: At least one test failed");
     }
+    printf("\n");
 
     return 0;
 }
