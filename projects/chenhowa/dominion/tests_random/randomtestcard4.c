a014dcff393bd9c78196d403e94f64a1cedd1ee7
Howard Chen <H_Chen@alumni.baylor.edu>
2018-02-16 18:30:52 -0800

    Started working on random tester for dominion.c: minionEffect

--- projects/chenhowa/dominion/tests_random/randomtestcard4.c
+++ projects/chenhowa/dominion/tests_random/randomtestcard4.c
@@ -1,7 +1,8 @@
 
 
+
 /* Howard Chen
- * Fuzzy tester for dominion.c: councilRoomEffect()
+ * Fuzzy tester for dominion.c: minionEffect()
  *
  *
  */
@@ -33,57 +34,15 @@ int checkCouncilRoomEffect(struct gameState* post, int player, int iteration) {
 
     //Check return value was correct (should always return 0)
     description = "Return was correct"; 
-    printf("Starting councilRoomEffect\n");
+    printf("Starting minionEffect\n");
     fflush(stdout);
-    ret = councilRoomEffect(post, player, 0);
-    printf("Ending councilRoomEffect\n");
+    ret = minionEffect(post, player, 0);
+    printf("Ending minionEffect\n");
     fflush(stdout);
     assertEq(0, ret, "return", description);
 
-    if(pre.deckCount[player] >= 4) {
-        printf("Case 1\n");
-
-        pre.handCount[player] += 4;
-        pre.hand[player][pre.handCount[player] - 1] = pre.deck[player][pre.deckCount[player] - 4];
-        pre.hand[player][pre.handCount[player] - 2] = pre.deck[player][pre.deckCount[player] - 3];
-        pre.hand[player][pre.handCount[player] - 3] = pre.deck[player][pre.deckCount[player] - 2];
-        pre.hand[player][pre.handCount[player] - 4] = pre.deck[player][pre.deckCount[player] - 1];
-        pre.deckCount[player] -= 4;
-        pre.numBuys += 1;
-
-        //Adjust the other player's values
-        pre.handCount[otherPlayer] += 1;
-        pre.hand[otherPlayer][pre.handCount[otherPlayer] - 1] = pre.deck[otherPlayer][pre.deckCount[otherPlayer] - 1];
-        pre.deckCount[otherPlayer] -= 1;
-
-        description = "No other changes have occurred";
-        ret = memcmp(&pre, post, sizeof(struct gameState));
-        assertEq(0, ret, "memcmp return", description);
-    } else if (pre.deckCount[player] + pre.discardCount[player] >= 4) {
-        printf("Case 2\n");
-        pre.handCount[player] += 4;
-        pre.hand[player][pre.handCount[player] - 1] = post->hand[player][post->handCount[player] - 1];
-        pre.hand[player][pre.handCount[player] - 2] = post->hand[player][post->handCount[player] - 2];
-        pre.hand[player][pre.handCount[player] - 3] = post->hand[player][post->handCount[player] - 3];
-        pre.hand[player][pre.handCount[player] - 4] = post->hand[player][post->handCount[player] - 4];
-        pre.deckCount[player] = pre.deckCount[player] + pre.discardCount[player] - 4;
-        memcpy(pre.deck[player], post->deck[player], pre.deckCount[player] + 4);
-        pre.discardCount[player] = post->discardCount[player];
-        memcpy(pre.deck[player], post->deck[player], pre.discardCount[player]);
-
-        //Adjust the other player's values
-        pre.handCount[otherPlayer] += 1;
-        pre.hand[otherPlayer][pre.handCount[otherPlayer] - 1] = pre.deck[otherPlayer][pre.deckCount[otherPlayer] - 1];
-        pre.deckCount[otherPlayer] -= 1;
 
 
-        description = "No other changes were made to game";
-        ret= memcmp(&pre, post, sizeof(struct gameState));
-        assertEq(0, ret, "memcmp return", description);
-    
-    } else {
-        printf("Case 3: Too few cards. Not interesting\n");
-    }
 
     fflush(stdout);
 
@@ -103,7 +62,7 @@ int main() {
     int i, n, p;
     int otherPlayer;
     struct gameState G;
-    printf ("Testing councilRoomEffect.\n");
+    printf ("Testing minionEffect.\n");
     printf ("RANDOM TESTS.\n");
     SelectStream(2);
     PutSeed(3);
@@ -113,8 +72,8 @@ int main() {
         for (i = 0; i < sizeof(struct gameState); i++) {
             ((char*)&G)[i] = floor(Random() * 256);
         }
-        G.numPlayers = 2; //Set standard value of 2 to avoid any issues.
         p = floor(Random() * 2);
+        G.numPlayers = 2; //Set standard value of 2 to avoid any issues.
         G.deckCount[p] = floor(Random() * (MAX_DECK));
         G.discardCount[p] = floor(Random() * (MAX_DECK - 5 - G.deckCount[p])); // ensure no overflow
         G.handCount[p] = floor(Random() * (MAX_HAND - 5)); //ensure no oveflow
@@ -130,6 +89,7 @@ int main() {
         //Make the played card count reasonable.
         G.playedCardCount = floor(Random() * (MAX_DECK - 5));
 
+
         //REPEAT FOR THE SECOND PLAYER
         otherPlayer = (p + 1) % 2;
         otherPlayer = floor(Random() * 2);
@@ -143,6 +103,8 @@ int main() {
             G.discard[otherPlayer][i] = floor(Random() * treasure_map); 
         }
         G.playedCardCount = floor(Random() * (MAX_DECK - 5));
+
+
 
         checkCouncilRoomEffect(&G, p, n);
     }
